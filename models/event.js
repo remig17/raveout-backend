e4961eaf6fe79a20170d19d7920f14bd5c6ae956
Camy <camyms13@gmail.com>
2023-05-15 15:19:16 +0200

    rdespe

--- models/event.js
+++ models/event.js
@@ -1,18 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const eventSchema = mongoose.Schema({
-    lieu: String,
-    date_debut: Date,
-    hdate_fin: Date,
-    photo: String,
-    organisateur: String,
-    Longitude: Number,
-    Latitude: Number,
-    tags: [String],
-    tickets: [{ type: mongoose.Schema.Types.ObjectId, ref: 'tickets' }]
-  });
-  
-  const Event = mongoose.model('events', eventSchema);
+  lieu: String,
+  date_debut: Date,
+  date_fin: Date,
+  photo: String,
+  organisateur: String,
+  Longitude: Number,
+  Latitude: Number,
+  tags: [String],
+  tickets: [{ type: mongoose.Schema.Types.ObjectId, ref: "tickets" }],
+});
 
-  module.exports = Event;
-  
+const Event = mongoose.model("events", eventSchema);
+
+module.exports = Event;
