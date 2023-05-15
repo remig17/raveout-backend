9a4d0b181a8763cd92a76e88c75a8500aa1ba9dd
Camy <camyms13@gmail.com>
2023-05-15 17:33:30 +0200

    oui

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+var eventsRouter = require("./routes/events");
 
 var app = express();
 const cors = require("cors");
@@ -19,5 +20,6 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/events", eventsRouter);
 
 module.exports = app;
--- routes/events.js
+++ routes/events.js
@@ -6,38 +6,50 @@ const Event = require("../models/events");
 const { checkBody } = require("../modules/checkBody");
 const uid2 = require("uid2");
 
-// router.post("/addEvent", (req, res) => {
-//   // Vérifie que les champs soient correctement remplies
-//   if (!checkBody(req.body, ["pseudo", "email", "password"])) {
-//     res.json({ result: false, error: "Missing or empty fields" });
-//     return;
-//   }
+router.post("/addEvent", (req, res) => {
+  // Vérifie que les champs soient correctement remplies
+  if (
+    !checkBody(req.body, [
+      "name",
+      "lieu",
+      "date_debut",
+      "date_fin",
+      "photo",
+      "organisateur",
+      "tags",
+    ])
+  ) {
+    res.json({ result: false, error: "Missing or empty fields" });
+    return;
+  }
 
-//   // Vérifie si l utilisateur est deja inscrit, s'il ne l'est pas, crée un nouvel utilisateur
-//   Event.findOne({ name: { $regex: new RegExp(req.body.name, "i") } }).then(
-//     (data) => {
-//       if (data === null) {
-//         const newUser = new User({
-//           pseudo: req.body.pseudo,
-//           email: req.body.email,
-//           token: uid2(32),
-//           password: hash,
-//           avatar: "",
-//           ville: "",
-//           styles_musicaux: "",
-//           tickets: [],
-//           like: [],
-//         });
+  // Vérifie si l utilisateur est deja inscrit, s'il ne l'est pas, crée un nouvel utilisateur
+  Event.findOne({ name: { $regex: new RegExp(req.body.name, "i") } }).then(
+    (data) => {
+      if (data === null) {
+        const newEvent = new Event({
+          name: req.body.name,
+          token: uid2(32),
+          lieu: req.body.lieu,
+          date_debut: req.body.date_debut,
+          date_fin: req.body.date_fin,
+          photo: req.body.photo,
+          organisateur: req.body.organisateur,
+          Longitude: "",
+          Latitude: "",
+          tags: [req.body.tags],
+          tickets: [],
+        });
 
-//         newUser.save().then((newUser) => {
-//           res.json({ result: true, token: newUser.token });
-//         });
-//       } else {
-//         // L'utilisateur existe déja dans la BDD
-//         res.json({ result: false, error: "User already exists" });
-//       }
-//     }
-//   );
-// });
+        newEvent.save().then((newEvent) => {
+          res.json({ result: true, token: newEvent.token });
+        });
+      } else {
+        // L'utilisateur existe déja dans la BDD
+        res.json({ result: false, error: "Event already exists" });
+      }
+    }
+  );
+});
 
 module.exports = router;
