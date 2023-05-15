b672e24c1be2d190732027cc191e0076b2dd92b4
Camy <camyms13@gmail.com>
2023-05-15 11:49:30 +0200

    routeuser

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,64 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+require("../models/connection");
+const User = require("../models/user");
+const { checkBody } = require("../modules/checkBody");
+const uid2 = require("uid2");
+const bcrypt = require("bcrypt");
+
+router.post("/signup", (req, res) => {
+  // Vérifie que les champs soient correctement remplies
+  if (!checkBody(req.body, ["prenom", "email", "password"])) {
+    res.json({ result: false, error: "Missing or empty fields" });
+    return;
+  }
+
+  // Vérifie si l utilisateur est deja inscrit, s'il ne l'est pas, crée un nouvel utilisateur
+  User.findOne({ username: req.body.username }).then((data) => {
+    if (data === null) {
+      const hash = bcrypt.hashSync(req.body.password, 10);
+
+      const newUser = new User({
+        prenom: req.body.prenom,
+        email: req.body.email,
+        token: uid2(32),
+        password: hash,
+        avatar: "",
+        ville: "",
+        styles_musicaux: "",
+        tickets: [],
+        like: [],
+      });
+
+      newUser.save().then((newUser) => {
+        res.json({ result: true, token: newUser.token });
+      });
+    } else {
+      // L'utilisateur existe déja dans la BDD
+      res.json({ result: false, error: "User already exists" });
+    }
+  });
 });
 
+// Vérifie que les champs soient correctement remplies
+router.post("/signin", (req, res) => {
+  if (!checkBody(req.body, ["email", "password"])) {
+    res.json({ result: false, error: "Missing or empty fields" });
+    return;
+  }
+  // Vérifie si l'email et le mot de passe correspondent a des données dans la BDD
+  User.findOne({ email: req.body.email }).then((data) => {
+    if (data && bcrypt.compareSync(req.body.password, data.password)) {
+      res.json({ result: true, token: data.token, prenom: data.prenom });
+    } else {
+      // Soit l utilisateur n'existe pas, soit le mot de passe est faux, renvoie un message d erreur
+      res.json({ result: false, error: "User not found or wrong password" });
+    }
+  });
+});
+
+router.get("/delete", (req, res) => {
+  User.deleteMany().then(() => console.log("database clear"));
+});
 module.exports = router;
